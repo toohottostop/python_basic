7ba2a9b4b72e134054bcf140a8ed43b76739ccb8
toohottostop <polezaevkolka@gmail.com>
2020-10-22 17:17:56 +0500

    add check of user input

--- bulls_and_cows_client.py
+++ bulls_and_cows_client.py
@@ -1,18 +1,37 @@
-from bulls_and_cows_engine import _numbers, make_number, number_check
+from bulls_and_cows_engine import make_new_number, number_check
 
-make_number()
+
+def check_user_input():
+    while True:
+        user_input = input('Please enter a four-digit number: ')
+        user_input_set = set(user_input)
+        if not user_input.isnumeric():
+            print('The string does not consist of numbers!')
+            continue
+        elif user_input[0] == '0':
+            print('Number cannot start from zero!')
+            continue
+        elif len(user_input_set) != 4:
+            print('There should be 4 numbers and they should not be repeated!')
+            continue
+        else:
+            break
+    return user_input
+
+
+make_new_number()
 tries = 0
-user_input = None
-while user_input != _numbers:
-    user_input = input('Please enter a four-digit number: ')
+while True:
+    user_input = check_user_input()
+    tries += 1
     number_check(user_input)
     tries += 1
-    if user_input == _numbers:
+    if number_check(user_input) == 4:
         print('You WIN!', '\nNumber of strokes: ', tries)
         restart = input('Continue? [Y/N]: ')
         if restart in ['Y', 'y']:
-            _numbers = ''
-            make_number()
+            tries = 0
+            make_new_number()
             continue
         else:
             break
