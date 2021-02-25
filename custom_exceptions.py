fbfe8085ae21e8a6d641d209ba4829b719390fae
toohottostop <polezaevkolka@gmail.com>
2021-02-25 22:28:44 +0500

    update

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -36,7 +36,7 @@ def one_day():
         current_carma = random.randint(1, 7)
         return current_carma
     else:
-        raise CustomError
+        raise random.choice(errors)
 
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
@@ -49,11 +49,13 @@ errors = [IamGodError('Hero GOD'),
           DepressionError('Hero in depression'),
           SuicideError('Hero self-destructed')]
 
-while current_carma <= ENLIGHTENMENT_CARMA_LEVEL:
-    try:
-        current_carma += one_day()
-    except CustomError:
-        cprint(f'Error: {random.choice(errors)}', color='red')
-    day += 1
-    print(f'Сurrent amount of karma: {current_carma}')
-cprint(f'It took the hero {day} days to enlighten', color='green')
+with open('errors.log', 'w', encoding='utf8') as file:
+    while current_carma <= ENLIGHTENMENT_CARMA_LEVEL:
+
+        try:
+            current_carma += one_day()
+        except CustomError as exc:
+            file.write(f'Ошибка: {exc}\n')
+        day += 1
+        print(f'Сurrent amount of karma: {current_carma}')
+    cprint(f'It took the hero {day} days to enlighten', color='green')
