09673843395347eb09e1ad5f91f1860b291055e2
toohottostop <polezaevkolka@gmail.com>
2021-06-17 22:59:05 +0500

    add console parser for tournament handler

--- bowling_tournament/score_console.py
+++ bowling_tournament/score_console.py
@@ -1,6 +1,7 @@
 import argparse
 
 from score_count_engine import Bowling, FirstThrowState
+from tournament_handler import TournamentHandler
 
 """Counting according to the old rules"""
 # parser = argparse.ArgumentParser(description='Fill the ticket')
@@ -13,14 +14,26 @@ from score_count_engine import Bowling, FirstThrowState
 # print(f'{game_result} - {bowling.total_points}')
 
 
-"""Counting according to the new rules"""
-parser = argparse.ArgumentParser(description='Fill the ticket')
-parser.add_argument('-result', type=str, help='enter your result')
-parser.add_argument('-count_rules', type=str, help='choose how to count: old rules or new rules')
-args = parser.parse_args('-result 3532X332/3/62--63X -count_rules new'.split())
+# """Counting according to the new rules"""
+# parser = argparse.ArgumentParser(description='Fill the ticket')
+# parser.add_argument('-result', type=str, help='enter your result')
+# parser.add_argument('-count_rules', type=str, help='choose how to count: old rules or new rules')
+# args = parser.parse_args('-result 3532X332/3/62--63X -count_rules new'.split())
+#
+# game_result = args.result
+# count_rules = args.count_rules
+# bowling = Bowling(game_result=game_result, state=FirstThrowState(), count_rules=count_rules)
+# bowling.analize_result()
+# print(f'{game_result} - {bowling.total_points}')
 
-game_result = args.result
-count_rules = args.count_rules
-bowling = Bowling(game_result=game_result, state=FirstThrowState(), count_rules=count_rules)
-bowling.analize_result()
-print(f'{game_result} - {bowling.total_points}')
+
+"""Count scores in tournament_scores.txt"""
+parser = argparse.ArgumentParser(description='Fill the ticket')
+parser.add_argument('-input', type=str, help='enter tournament file name')
+parser.add_argument('-output', type=str, help='enter tournament results file name')
+parser.add_argument('-count_rules', type=str, help='choose how to count: old rules or new rules')
+args = parser.parse_args('-input tournament_scores.txt -output tournament_result.txt -count_rules new'.split())
+
+input_file_name, output_file_name, count_rules = args.input, args.output, args.count_rules
+handler = TournamentHandler(input_file_name=input_file_name, output_file_name=output_file_name, count_rules=count_rules)
+handler.run()
